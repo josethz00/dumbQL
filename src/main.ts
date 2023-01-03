2d2442a94e8980807ec385accfcdae15b209fe07
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-01-03 11:46:12 -0300

    feat: add stronger types to db

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,27 @@
 import repl from 'repl';
 import DumbQL from './dumbql';
+import Document from './document';
 
 const dumbQL = new DumbQL();
 
-dumbQL.connectToDatabase('myfirstdumbqldb');
+const documents = dumbQL.connectToDatabase('myfirstdumbqldb');
+
+const db: {
+  [key: typeof documents[number]]: Document;
+} = documents.reduce(
+  (
+    acc: {
+      [key: typeof documents[number]]: Document;
+    },
+    docName: string,
+  ) => {
+    acc[docName] = new Document(docName);
+    return acc;
+  },
+  {},
+);
 
 const replCtx = repl.start('dumbQL > ').context;
 replCtx.createDatabase = dumbQL.createDatabase;
 replCtx.connectToDatabase = dumbQL.connectToDatabase;
-replCtx.db = dumbQL.documents;
+replCtx.db = db;
